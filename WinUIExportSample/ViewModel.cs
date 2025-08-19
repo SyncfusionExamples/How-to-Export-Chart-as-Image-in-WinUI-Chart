aaaabbed581b7daa756d67fc616e439beb52c371
NitheeshKumarThangaraj <nitheeshkumar.t@syncfusion.com>
2025-08-19 11:18:43 +0530

    Updated the new sample for export chart

--- WinUIExportSample/ViewModel.cs
+++ WinUIExportSample/ViewModel.cs
@@ -1,8 +1,7 @@
-﻿
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
-namespace WinUISampleDemo
+namespace WinUIExportSample
 {
     public class ViewModel
     {
